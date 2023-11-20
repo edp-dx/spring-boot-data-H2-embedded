0733c417bee010dccbe2d33400e03ce1e69aa62e
Vadim Vlasenko <33458204+vadimvlasenko@users.noreply.github.com>
2023-11-20 16:20:10 +0200

    Update SpringApp.java

--- src/main/java/com/iamvickyav/springboot/SpringBootRestWithH2/SpringApp.java
+++ src/main/java/com/iamvickyav/springboot/SpringBootRestWithH2/SpringApp.java
@@ -1,11 +1,11 @@
 package com.iamvickyav.springboot.SpringBootRestWithH2;
 
-import com.iamvickyav.springboot.SpringBootRestWithH2.service.SponsorService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.iamvickyav.springboot.SpringBootRestWithH2.service")
 public class SpringApp {
 
 	public static void main(String[] args) {
